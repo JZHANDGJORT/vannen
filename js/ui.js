9ee055c59d4f66a6a7c6f6fbc047ff34d314d339
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-07-19 14:01:30 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,10 @@
 function renderActions() {
+function renderActions() {
 
+    alert("renderActions körs");
+
+    const actions =
+        document.getElementById("actions");
     const actions =
         document.getElementById("actions");
 
