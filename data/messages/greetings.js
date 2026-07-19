82aa7875d69959bac9bf268d542d89b301a2a7a4
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-07-19 12:58:53 +0200

    Update greetings.js

--- data/messages/greetings.js
+++ data/messages/greetings.js
@@ -1,1 +1,82 @@
+const greetings = {
 
+    otis: [
+
+        {
+            text:
+                "Hej! 🌊 Vad roligt att du tittade förbi. Jag låg precis och flöt en stund och tänkte på hur skönt det är att bara ta det lugnt ibland."
+        },
+
+        {
+            text:
+                "Där är du ju! 🦦 Jag blev glad när jag såg dig komma."
+        },
+
+        {
+            text:
+                "Hej! Jag hittade en liten fin sten idag. Jag undrar vem som hade hittat den först, du eller jag?"
+        },
+
+        {
+            text:
+                "Vad fint att du kom förbi en stund. Jag tycker om när vi får ses så här."
+        },
+
+        {
+            text:
+                "Hej! Jag har varit ute och utforskat lite vid vattnet idag. Det finns alltid något nytt att upptäcka."
+        },
+
+        {
+            text:
+                "Där är du! Jag låg på rygg och tittade på molnen en stund. Har du någon gång gjort det?"
+        },
+
+        {
+            text:
+                "Hej hej! 🌊 Jag hoppas att din dag har varit snäll mot dig."
+        },
+
+        {
+            text:
+                "Jag är glad att du kom. Vill du bara vara här en liten stund tillsammans med mig?"
+        },
+
+        {
+            text:
+                "Hej! Idag hittade jag tre olika stenar som såg helt olika ut. Jag kunde inte bestämma vilken jag tyckte bäst om."
+        },
+
+        {
+            text:
+                "Vad roligt att ses igen. Jag har funderat på vad vi kanske kan upptäcka tillsammans idag."
+        },
+
+        {
+            text:
+                "Hej! Jag tog en liten simtur tidigare. Vattnet var precis lagom kallt och väldigt uppfriskande."
+        },
+
+        {
+            text:
+                "Där är du ju. Jag sparade en liten plats bredvid mig."
+        },
+
+        {
+            text:
+                "Hej! Ibland tror jag att de minsta sakerna kan bli de finaste. Som en fin sten, en solstråle eller en lugn stund."
+        },
+
+        {
+            text:
+                "Jag är glad att du kom förbi idag. Hur har du det?"
+        },
+
+        {
+            text:
+                "Hej! 🌊 Jag undrar vad vi ska hitta på tillsammans idag. Eller kanske bara vara en stund."
+        }
+
+    ]
+
+};
