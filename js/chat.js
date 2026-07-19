6d3e4107b2b48c94ae4c27bd96bbf4fa5d9904bc
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-07-19 13:16:00 +0200

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,39 @@
+function addMessage(text, sender) {
 
+    const messages =
+        document.getElementById("messages");
+
+
+    const message =
+        document.createElement("div");
+
+
+    message.classList.add(
+        "message",
+        sender
+    );
+
+
+    message.textContent =
+        text;
+
+
+    messages.appendChild(message);
+
+
+    scrollToBottom();
+
+}
+
+
+
+function scrollToBottom() {
+
+    const chat =
+        document.getElementById("chat");
+
+
+    chat.scrollTop =
+        chat.scrollHeight;
+
+}
