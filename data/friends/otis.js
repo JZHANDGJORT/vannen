acca5f8b340208d0a9d689e804d6358d061f0c9c
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-07-20 14:45:06 +0200

    Update otis.js

--- data/friends/otis.js
+++ data/friends/otis.js
@@ -43,7 +43,7 @@ const otis = {
         "Otis är en lekfull och nyfiken utter som tycker om både äventyr och lugna stunder. Han vill upptäcka världen tillsammans med dig och påminna om att små saker kan göra en dag fin.",
 
     about:
-    "Vad roligt att du vill veta lite mer om mig! 🌊 Jag heter Otis..."
+    "Vad roligt att du vill veta lite mer om mig! 🌊 Jag heter Otis...",
 
     purpose:
         "Jag hoppas att vi kan ta hand om varandra och göra dagarna lite mjukare.",
