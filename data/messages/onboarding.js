2757b5ea5113c461c2731b503b88942b5d74be8b
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-07-20 22:37:03 +0200

    Update onboarding.js

--- data/messages/onboarding.js
+++ data/messages/onboarding.js
@@ -1,1 +1,18 @@
+const onboarding = {
 
+    firstMeeting: {
+
+        greeting:
+            "Hej! 🌊 Jag heter Otis. Vad roligt att du hittade hit. Jag tror att vi kommer få många fina stunder tillsammans. Vad ska jag kalla dig?",
+
+
+        afterName:
+            "Vad fint att träffa dig, {name}. 💚 Jag vill gärna lära känna dig lite bättre.",
+
+
+        relationQuestion:
+            "Är jag en vän som mest är till för dig, eller vill du att jag ska få lära känna fler som betyder mycket för dig?"
+
+    }
+
+};
