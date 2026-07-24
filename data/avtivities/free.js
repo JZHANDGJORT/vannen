38d07c16a57bb6e00d7307540cbda6be83f35e76
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-07-25 00:04:32 +0200

    Update free.js

--- data/avtivities/free.js
+++ data/avtivities/free.js
@@ -2,9 +2,45 @@ const freeActivities = {
 
     indoor: [
 
+        {
+
+            id: "bracelet",
+
+            text:
+                "Vad sägs om att göra ett armband till någon du tycker om? 💚"
+
+        },
+
+        {
+
+            id: "drawing",
+
+            text:
+                "Hämta papper och pennor så hittar vi på något att rita."
+
+        }
+
     ],
 
     outdoor: [
+
+        {
+
+            id: "stones",
+
+            text:
+                "Jag har en idé! Ska vi leta efter tre fina stenar? 🌊"
+
+        },
+
+        {
+
+            id: "treasure",
+
+            text:
+                "Kan vi hitta fem små skatter ute i naturen?"
+
+        }
 
     ]
 
