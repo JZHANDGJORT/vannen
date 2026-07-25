9bb260031c6c52aef443103149494f6fa34b2114
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-07-25 23:25:12 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -480,7 +480,7 @@ function simpleActivity(type) {
         "Vad roligt! 🎨 Vet du redan vad du vill måla eller vill du ha en liten idé?",
 
         build:
-        "Vad ska vi bygga? Jag är nyfiken! 🧱",
+        "Vad roligt! 🧱 Vet du redan vad du vill bygga eller vill du ha en liten idé?",
 
         craft:
         "Pyssel är roligt! ✂️ Jag vill gärna se vad du gör.",
