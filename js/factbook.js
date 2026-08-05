fa7711178ea6526a2a2351e05478db9ae7e79177
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-08-05 21:00:04 +0200

    Update factbook.js

--- js/factbook.js
+++ js/factbook.js
@@ -1,1 +1,75 @@
+/*
+  Otis Faktabok
+*/
 
+
+const factBookData = {
+
+    id: "factbook",
+
+    title: "Otis Faktabok",
+
+
+    pages: [
+
+        // Sida 0 - innehållsförteckning
+        {
+            image: "",
+
+            background:
+            "images/Otis/otis-backpack-item-sagobok-openl.PNG",
+
+            backgroundClass:
+            "background-left",
+          
+            title: "",
+
+            text: "",
+
+            imageClass: ""
+        },
+
+
+        // Sida 1 - Kapitel 1
+        {
+            image:
+            "images/Otis/storybook/chapter01/page01.JPEG",
+
+            background:
+            "images/Otis/otis-backpack-item-sagobok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+          
+            title:
+`
+<span class="chapter-number">
+Kapitel 1
+</span>
+<br>
+
+<span class="chapter-name">
+Otis och den magiska stenen
+</span>
+`,
+
+            text:
+
+`
+<p>Långt inne bland träden, där skogen mötte det glittrande vattnet, bodde en liten utter som hette Otis.</p>
+
+<p>Otis var nyfiken på nästan allt.</p>
+`,
+
+            imageClass:
+            "chapter-image",
+
+            textClass:
+            "chapter-text"
+          
+        }
+
+
+    ]
+
+};
