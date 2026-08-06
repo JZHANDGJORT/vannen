41ff85596669ce79f65e60291612453445619203
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-08-07 01:18:10 +0200

    Update book.js

--- js/book.js
+++ js/book.js
@@ -3,7 +3,6 @@
 */
 
 
-
 let currentBookType = null;
 
 let currentBookPage = 0;
@@ -123,8 +122,12 @@ function updateBookPage() {
 
 
     const story =
-        currentBookData.pages[currentBookPage];
+    currentBookData.pages[currentBookPage];
 
+if (!story) {
+    console.error("Sidan hittades inte:", currentBookPage);
+    return;
+}
 
 
     background.src =
