af200a3e6e7c0037313125e05c5e179c4add3c0a
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-08-09 21:13:35 +0200

    Create recipebook.js

---
+++ js/recipebook.js
@@ -0,0 +1,51 @@
+/*
+  Otis Receptbok
+*/
+const recipeBookData = {
+    id: "recipebook",
+    title: "Otis Receptbok",
+    pages: [
+        // Sida 0 - Innehållsförteckning
+        {
+            image: "",
+            background:
+            "images/Otis/otis-backpack-item-receptbok-openl.PNG",
+            backgroundClass:
+            "background-left",
+            title: "",
+            text: "",
+            imageClass: "",
+            textClass: ""
+        },
+        // Sida 1 - Kryddmuffins
+        {
+            image:
+            "images/Otis/otis-recept-kryddmuffins.PNG",
+            background:
+            "images/Otis/otis-backpack-item-receptbok-openr.PNG",
+            backgroundClass:
+            "background-right",
+            title: "",
+            text: "",
+            imageClass:
+            "recipe-image-right",
+            textClass:
+            "recipe-text-right"
+        },
+        // Sida 2 - Pannkaksbröd
+        {
+            image:
+            "images/Otis/otis-recept-pannkaksbrod.PNG",
+            background:
+            "images/Otis/otis-backpack-item-receptbok-openl.PNG",
+            backgroundClass:
+            "background-left",
+            title: "",
+            text: "",
+            imageClass:
+            "recipe-image-left",
+            textClass:
+            "recipe-text-left"
+        }
+    ]
+};
