020296e1d561647a63849195e6df2e23b8e79cbe
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-08-10 12:56:55 +0200

    Update photoalbum.js

--- js/photoalbum.js
+++ js/photoalbum.js
@@ -1,1 +1,63 @@
-
+/*
+  Otis Fotoalbum
+*/
+const photoAlbumData = {
+    id: "photoalbum",
+    title: "Otis Fotoalbum",
+    pages: [
+        // Sida 1
+        {
+            background:
+                "images/Otis/otis-backpack-item-fotoalbum-openl.PNG",
+            backgroundClass:
+                "background-left",
+            photos: [
+                {
+                    image:
+                        "images/Otis/photoalbum/page01-photo01.PNG",
+                    class:
+                        "photo-1"
+                },
+                {
+                    image:
+                        "images/Otis/photoalbum/page01-photo02.PNG",
+                    class:
+                        "photo-2"
+                },
+                {
+                    image:
+                        "images/Otis/photoalbum/page01-photo03.PNG",
+                    class:
+                        "photo-3"
+                }
+            ]
+        },
+        // Sida 2
+        {
+            background:
+                "images/Otis/otis-backpack-item-fotoalbum-openr.PNG",
+            backgroundClass:
+                "background-right",
+            photos: [
+                {
+                    image:
+                        "images/Otis/photoalbum/page02-photo01.PNG",
+                    class:
+                        "photo-1"
+                },
+                {
+                    image:
+                        "images/Otis/photoalbum/page02-photo02.PNG",
+                    class:
+                        "photo-2"
+                },
+                {
+                    image:
+                        "images/Otis/photoalbum/page02-photo03.PNG",
+                    class:
+                        "photo-3"
+                }
+            ]
+        }
+    ]
+};
