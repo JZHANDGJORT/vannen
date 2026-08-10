59f7faaa0af923a05d0d38b48d58d420ef7cf8ba
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-08-10 13:16:51 +0200

    Update photoalbum.js

--- js/photoalbum.js
+++ js/photoalbum.js
@@ -5,7 +5,7 @@ const photoAlbumData = {
     id: "photoalbum",
     title: "Otis Fotoalbum",
     pages: [
-        // Sida 1
+        // Uppslag 1
         {
             background:
                 "images/Otis/otis-backpack-item-fotoalbum-openl.PNG",
@@ -15,47 +15,20 @@ const photoAlbumData = {
                 {
                     image:
                         "images/Otis/photoalbum/page01-photo01.PNG",
-                    class:
-                        "photo-1"
+                    className:
+                        "photoalbum-photo photoalbum-photo-01"
                 },
                 {
                     image:
                         "images/Otis/photoalbum/page01-photo02.PNG",
-                    class:
-                        "photo-2"
+                    className:
+                        "photoalbum-photo photoalbum-photo-02"
                 },
                 {
                     image:
                         "images/Otis/photoalbum/page01-photo03.PNG",
-                    class:
-                        "photo-3"
-                }
-            ]
-        },
-        // Sida 2
-        {
-            background:
-                "images/Otis/otis-backpack-item-fotoalbum-openr.PNG",
-            backgroundClass:
-                "background-right",
-            photos: [
-                {
-                    image:
-                        "images/Otis/photoalbum/page02-photo01.PNG",
-                    class:
-                        "photo-1"
-                },
-                {
-                    image:
-                        "images/Otis/photoalbum/page02-photo02.PNG",
-                    class:
-                        "photo-2"
-                },
-                {
-                    image:
-                        "images/Otis/photoalbum/page02-photo03.PNG",
-                    class:
-                        "photo-3"
+                    className:
+                        "photoalbum-photo photoalbum-photo-03"
                 }
             ]
         }
