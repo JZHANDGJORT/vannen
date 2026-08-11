8a10e09201a6d9e07ea574cd9cee1ee6a124e9d1
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-08-11 12:31:34 +0200

    Update photoalbum.js

--- js/photoalbum.js
+++ js/photoalbum.js
@@ -4,76 +4,105 @@
 const photoAlbumData = {
     id: "photoalbum",
     title: "Otis Fotoalbum",
+
     pages: [
+
         /*
-           Sida 1
+           =========================
+           SIDA 1 – VÄNSTER
+           =========================
         */
         {
             background:
                 "images/Otis/otis-backpack-item-fotoalbum-openl.PNG",
+
             backgroundClass:
                 "background-left",
+
             photos: [
                 {
                     image:
                         "images/Otis/photoalbum/page01-photo01.PNG",
+
                     class:
-                        "photoalbum-photo photoalbum-photo-01",
+                        "photoalbum-photo photoalbum-page-left-photo-01",
+
                     caption:
                         "En sommar för länge sedan"
                 },
+
                 {
                     image:
                         "images/Otis/photoalbum/page01-photo02.PNG",
+
                     class:
-                        "photoalbum-photo photoalbum-photo-02",
+                        "photoalbum-photo photoalbum-page-left-photo-02",
+
                     caption:
                         "En dag full av bus"
                 },
+
                 {
                     image:
                         "images/Otis/photoalbum/page01-photo03.PNG",
+
                     class:
-                        "photoalbum-photo photoalbum-photo-03",
+                        "photoalbum-photo photoalbum-page-left-photo-03",
+
                     caption:
                         "Dagen då jag hittade den"
                 }
             ]
         },
+
+
         /*
-           Sida 2
+           =========================
+           SIDA 2 – HÖGER
+           =========================
         */
         {
             background:
                 "images/Otis/otis-backpack-item-fotoalbum-openr.PNG",
+
             backgroundClass:
                 "background-right",
+
             photos: [
                 {
                     image:
                         "images/Otis/photoalbum/page02-photo01.PNG",
+
                     class:
-                        "photoalbum-photo photoalbum-photo-01",
+                        "photoalbum-photo photoalbum-page-right-photo-01",
+
                     caption:
                         "Mitt första äventyr"
                 },
+
                 {
                     image:
                         "images/Otis/photoalbum/page02-photo02.PNG",
+
                     class:
-                        "photoalbum-photo photoalbum-photo-02",
+                        "photoalbum-photo photoalbum-page-right-photo-02",
+
                     caption:
                         "En vanlig dag med Bosse"
                 },
+
                 {
                     image:
                         "images/Otis/photoalbum/page02-photo03.PNG",
+
                     class:
-                        "photoalbum-photo photoalbum-photo-03",
+                        "photoalbum-photo photoalbum-page-right-photo-03",
+
                     caption:
                         "Min favoritplats"
                 }
             ]
         }
+
     ]
 };
