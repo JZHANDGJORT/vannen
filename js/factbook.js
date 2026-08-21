31d1fa6d108a555e2385b8ac008ae2091a99b5c3
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-08-21 23:52:22 +0200

    Update factbook.js

--- js/factbook.js
+++ js/factbook.js
@@ -1,1 +1,511 @@
+/*
+  Otis Faktabok
+*/
 
+
+const factBookData = {
+
+    id: "factbook",
+
+    title: "Otis Faktabok",
+
+
+    pages: [
+
+
+        // ========================================
+        // Sida 0 - Innehållsförteckning
+        // ========================================
+
+        {
+            image: "",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openl.PNG",
+
+            backgroundClass:
+            "background-left",
+
+            title: "",
+
+            text: "",
+
+            imageClass: ""
+        },
+
+
+        // ========================================
+        // KAPITEL 1 – UTTRAR
+        // ========================================
+
+
+        // Sida 1 - Kapitelstart
+        {
+            image:
+            "images/Otis/factbook/chapter01/page01.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+
+            title:
+`
+<span class="chapter-number">
+Kapitel 1
+</span>
+<br>
+
+<span class="chapter-name">
+Uttrar
+</span>
+`,
+
+            text:
+`
+<p>Hej! 🦦</p>
+
+<p>Jag heter Otis och jag älskar att upptäcka saker vid vattnet.</p>
+
+<p>Visste du att uttrar finns på många platser i världen? Här kommer några spännande fakta!</p>
+`,
+
+            imageClass:
+            "fact-chapter-image",
+
+            textClass:
+            "fact-chapter-text"
+
+        },
+
+
+        // Sida 2 - Fantastiska simmare
+        {
+            image:
+            "images/Otis/factbook/chapter01/page02.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openl.PNG",
+
+            backgroundClass:
+            "background-left",
+
+            title: "",
+
+            text:
+`
+<p>Uttrar är riktiga vattenälskare. 🌊</p>
+
+<p>Deras långa kropp och starka svans hjälper dem att simma snabbt genom vattnet.</p>
+
+<p>De kan dyka efter mat och hålla andan under flera minuter.</p>
+`,
+
+            imageClass:
+            "fact-image-left",
+
+            textClass:
+            "fact-text-left"
+
+        },
+
+
+        // Sida 3 - Den mjuka pälsen
+        {
+            image:
+            "images/Otis/factbook/chapter01/page03.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+
+            title: "",
+
+            text:
+`
+<p>En utterpäls är en av de tätaste pälsarna i hela djurriket.</p>
+
+<p>Under pälsen finns massor av små hårstrån som hjälper uttern att hålla värmen när den simmar i kallt vatten.</p>
+
+<p>Därför är det viktigt att uttrar tar hand om sin päls och håller den ren.</p>
+`,
+
+            imageClass:
+            "fact-image-right",
+
+            textClass:
+            "fact-text-right"
+
+        },
+
+
+        // Sida 4 - Favoritstenen
+        {
+            image:
+            "images/Otis/factbook/chapter01/page04.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openl.PNG",
+
+            backgroundClass:
+            "background-left",
+
+            title: "",
+
+            text:
+`
+<p>Vissa havsuttrar använder en favoritsten som ett litet verktyg. 🖤</p>
+
+<p>De kan använda stenen för att öppna hårda skal från musslor och andra smådjur.</p>
+
+<p>Precis som Otis tycker många uttrar om fina stenar!</p>
+`,
+
+            imageClass:
+            "fact-image-left",
+
+            textClass:
+            "fact-text-left"
+
+        },
+
+
+        // Sida 5 - Lekfulla uttrar
+        {
+            image:
+            "images/Otis/factbook/chapter01/page05.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+
+            title: "",
+
+            text:
+`
+<p>Uttrar är nyfikna och lekfulla djur.</p>
+
+<p>De kan rulla runt, jaga varandra och busa i vattnet.</p>
+
+<p>Lek hjälper dem att lära sig viktiga saker och hålla sig aktiva.</p>
+`,
+
+            imageClass:
+            "fact-image-right",
+
+            textClass:
+            "fact-text-right"
+
+        },
+
+
+        // Sida 6 - Var bor uttrar?
+        {
+            image:
+            "images/Otis/factbook/chapter01/page06.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openl.PNG",
+
+            backgroundClass:
+            "background-left",
+
+            title: "",
+
+            text:
+`
+<p>Uttrar finns på många platser i världen.</p>
+
+<p>De kan leva vid sjöar, floder och hav.</p>
+
+<p>Där det finns rent vatten och gott om mat kan en utter hitta ett hem.</p>
+`,
+
+            imageClass:
+            "fact-image-left page-6",
+
+            textClass:
+            "fact-text-left"
+
+        },
+
+
+        // Sida 7 - Avslutning
+        {
+            image:
+            "images/Otis/factbook/chapter01/page07.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+
+            title: "",
+
+            text:
+`
+<p>Nu vet du lite mer om uttrar! 🌿</p>
+
+<p>Nästa gång du ser en bäck, en sjö eller havet kan du tänka på alla små äventyr som händer under vattenytan.</p>
+
+<p>/ Otis 🦦</p>
+`,
+
+            imageClass:
+            "fact-image-right",
+
+            textClass:
+            "fact-text-right"
+
+        },
+
+
+        // ========================================
+        // KAPITEL 2 – GRÄVLINGAR
+        // ========================================
+
+
+        // Sida 8 - Kapitelstart
+        {
+            image:
+            "images/Otis/factbook/chapter02/page01.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+
+            title:
+`
+<span class="chapter-number">
+Kapitel 2
+</span>
+<br>
+
+<span class="chapter-name">
+Grävlingar
+</span>
+`,
+
+            text:
+`
+<p>Hej igen! 🦡</p>
+
+<p>Nu ska vi lära känna ett annat djur som finns ute i naturen.</p>
+
+<p>Grävlingen är duktig på att gräva och trivs bäst när det börjar bli kväll.</p>
+
+<p>Här kommer några spännande fakta om grävlingar!</p>
+`,
+
+            imageClass:
+            "fact-chapter-image",
+
+            textClass:
+            "fact-chapter-text"
+
+        },
+
+
+        // Sida 9 - Den randiga masken
+        {
+            image:
+            "images/Otis/factbook/chapter02/page02.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openl.PNG",
+
+            backgroundClass:
+            "background-left",
+
+            title: "",
+
+            text:
+`
+<p>Det är lätt att känna igen en grävling.</p>
+
+<p>Den har en kraftig kropp, korta ben och en svartvit randig teckning över huvudet.</p>
+
+<p>De långa klorna är perfekta när grävlingen behöver gräva.</p>
+`,
+
+            imageClass:
+            "fact-image-left",
+
+            textClass:
+            "fact-text-left"
+
+        },
+
+
+        // Sida 10 - Grävlingens hem
+        {
+            image:
+            "images/Otis/factbook/chapter02/page03.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+
+            title: "",
+
+            text:
+`
+<p>Grävlingen bor ofta i ett stort gryt under marken. 🌿</p>
+
+<p>Med sina starka klor kan den gräva gångar och rum där den kan vila och ta skydd.</p>
+
+<p>Ett gryt kan användas av grävlingar under många år och ha flera olika ingångar.</p>
+`,
+
+            imageClass:
+            "fact-image-right",
+
+            textClass:
+            "fact-text-right"
+
+        },
+
+
+        // Sida 11 - Vad äter grävlingen?
+        {
+            image:
+            "images/Otis/factbook/chapter02/page04.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openl.PNG",
+
+            backgroundClass:
+            "background-left",
+
+            title: "",
+
+            text:
+`
+<p>Grävlingen äter lite av varje. 🍎</p>
+
+<p>Den kan äta maskar, insekter, smådjur, bär och annan mat som den hittar i naturen.</p>
+
+<p>Grävlingen är alltså en allätare – precis som många andra djur som kan hitta mat på många olika sätt.</p>
+`,
+
+            imageClass:
+            "fact-image-left",
+
+            textClass:
+            "fact-text-left"
+
+        },
+
+
+        // Sida 12 - En riktig grävare
+        {
+            image:
+            "images/Otis/factbook/chapter02/page05.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+
+            title: "",
+
+            text:
+`
+<p>Grävlingen är en riktig grävare! 🐾</p>
+
+<p>De starka klorna hjälper den att gräva efter mat och bygga sitt gryt.</p>
+
+<p>När den letar efter mat kan den snabbt gräva sig ner i jorden.</p>
+`,
+
+            imageClass:
+            "fact-image-right",
+
+            textClass:
+            "fact-text-right"
+
+        },
+
+
+        // Sida 13 - När solen går ner
+        {
+            image:
+            "images/Otis/factbook/chapter02/page06.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openl.PNG",
+
+            backgroundClass:
+            "background-left",
+
+            title: "",
+
+            text:
+`
+<p>Grävlingen är mest aktiv på kvällen och natten. 🌙</p>
+
+<p>När det börjar bli mörkt lämnar den ofta sitt gryt för att leta efter mat.</p>
+
+<p>Grävlingen har ett bra luktsinne som hjälper den att hitta sådant som gömmer sig i marken.</p>
+`,
+
+            imageClass:
+            "fact-image-left",
+
+            textClass:
+            "fact-text-left"
+
+        },
+
+
+        // Sida 14 - Små grävlingar
+        {
+            image:
+            "images/Otis/factbook/chapter02/page07.PNG",
+
+            background:
+            "images/Otis/otis-backpack-item-faktabok-openr.PNG",
+
+            backgroundClass:
+            "background-right",
+
+            title: "",
+
+            text:
+`
+<p>Grävlingens ungar föds inne i grytet. 🦡</p>
+
+<p>När de blir större börjar de försiktigt upptäcka världen utanför.</p>
+
+<p>Så småningom lär de sig att hitta mat, gräva och klara sig själva.</p>
+
+<p>Nu vet du lite mer om grävlingar!</p>
+
+<p>Kanske finns det en grävling ute i skogen just nu, precis någonstans där du inte kan se den.</p>
+
+<p>/ Otis 🦦</p>
+`,
+
+            imageClass:
+            "fact-image-right",
+
+            textClass:
+            "fact-text-right"
+
+        }
+
+
+    ]
+
+};
