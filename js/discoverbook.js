cb1e1b9596ecacf419bb5fac019c372787be6d81
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-09-03 11:41:42 +0200

    Create discoverbook.js

---
+++ js/discoverbook.js
@@ -0,0 +1,83 @@
+/*
+  Otis Upptäckarbok
+*/
+
+const discoverBookData = {
+    id: "discoverbook",
+    title: "Otis Upptäckarbok",
+    pages: [
+
+        // Sida 0 - Innehållsförteckning
+        {
+            image: "",
+            background:
+            "images/Otis/otis-backpack-item-upptackarbok-openl.PNG",
+            backgroundClass:
+            "background-left",
+            title: "",
+            text: "",
+            imageClass: "",
+            textClass: ""
+        },
+
+        // Sida 1
+        {
+            image: "",
+            background:
+            "images/Otis/otis-backpack-item-upptackarbok-openr.PNG",
+            backgroundClass:
+            "background-right",
+            title: "",
+            text: "",
+            imageClass:
+            "discover-image-right",
+            textClass:
+            "discover-text-right"
+        },
+
+        // Sida 2
+        {
+            image: "",
+            background:
+            "images/Otis/otis-backpack-item-upptackarbok-openl.PNG",
+            backgroundClass:
+            "background-left",
+            title: "",
+            text: "",
+            imageClass:
+            "discover-image-left",
+            textClass:
+            "discover-text-left"
+        },
+
+        // Sida 3
+        {
+            image: "",
+            background:
+            "images/Otis/otis-backpack-item-upptackarbok-openr.PNG",
+            backgroundClass:
+            "background-right",
+            title: "",
+            text: "",
+            imageClass:
+            "discover-image-right",
+            textClass:
+            "discover-text-right"
+        },
+
+        // Sida 4
+        {
+            image: "",
+            background:
+            "images/Otis/otis-backpack-item-upptackarbok-openl.PNG",
+            backgroundClass:
+            "background-left",
+            title: "",
+            text: "",
+            imageClass:
+            "discover-image-left",
+            textClass:
+            "discover-text-left"
+        }
+    ]
+};
