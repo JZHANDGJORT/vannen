d43d734e6d70c791d122e04b26c726ee985d2b5e
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-09-12 01:12:46 +0200

    Update photoalbum.js

--- js/photoalbum.js
+++ js/photoalbum.js
@@ -1,108 +1,236 @@
 /*
-   Otis Fotoalbum
+   Fotoalbum
 */
 const photoAlbumData = {
-    id: "photoalbum",
-    title: "Otis Fotoalbum",
-
-    pages: [
-
-        /*
-           =========================
-           SIDA 1 – VÄNSTER
-           =========================
-        */
-        {
-            background:
-                "images/Otis/photoalbum/otis-backpack-item-fotoalbum-openl.PNG",
-
-            backgroundClass:
-                "background-left",
-
-            photos: [
-                {
-                    image:
-                        "images/Otis/photoalbum/page01-photo01.PNG",
-
-                    class:
-                        "photoalbum-photo photoalbum-page-left-photo-01",
-
-                    caption:
-                        "En sommar för länge sedan"
-                },
-
-                {
-                    image:
-                        "images/Otis/photoalbum/page01-photo02.PNG",
-
-                    class:
-                        "photoalbum-photo photoalbum-page-left-photo-02",
-
-                    caption:
-                        "En dag full av bus"
-                },
-
-                {
-                    image:
-                        "images/Otis/photoalbum/page01-photo03.PNG",
-
-                    class:
-                        "photoalbum-photo photoalbum-page-left-photo-03",
-
-                    caption:
-                        "Dagen då jag hittade den"
-                }
-            ]
-        },
 
 
-        /*
-           =========================
-           SIDA 2 – HÖGER
-           =========================
-        */
-        {
-            background:
-                "images/Otis/photoalbum/otis-backpack-item-fotoalbum-openr.PNG",
+    /*
+       =========================
+       OTIS
+       =========================
+    */
+    otis01: {
 
-            backgroundClass:
-                "background-right",
+        id: "photoalbum",
+        title: "Otis Fotoalbum",
 
-            photos: [
-                {
-                    image:
-                        "images/Otis/photoalbum/page02-photo01.PNG",
+        pages: [
 
-                    class:
-                        "photoalbum-photo photoalbum-page-right-photo-01",
+            /*
+               =========================
+               SIDA 1 – VÄNSTER
+               =========================
+            */
+            {
+                background:
+                    "images/Otis/photoalbum/otis-backpack-item-fotoalbum-openl.PNG",
 
-                    caption:
-                        "Mitt första äventyr"
-                },
+                backgroundClass:
+                    "background-left",
 
-                {
-                    image:
-                        "images/Otis/photoalbum/page02-photo02.PNG",
+                photos: [
+                    {
+                        image:
+                            "images/Otis/photoalbum/page01-photo01.PNG",
 
-                    class:
-                        "photoalbum-photo photoalbum-page-right-photo-02",
+                        class:
+                            "photoalbum-photo photoalbum-page-left-photo-01",
 
-                    caption:
-                        "En vanlig dag med Bosse"
-                },
+                        caption:
+                            "En sommar för länge sedan"
+                    },
 
-                {
-                    image:
-                        "images/Otis/photoalbum/page02-photo03.PNG",
+                    {
+                        image:
+                            "images/Otis/photoalbum/page01-photo02.PNG",
 
-                    class:
-                        "photoalbum-photo photoalbum-page-right-photo-03",
+                        class:
+                            "photoalbum-photo photoalbum-page-left-photo-02",
 
-                    caption:
-                        "Min favoritplats"
-                }
-            ]
-        }
+                        caption:
+                            "En dag full av bus"
+                    },
 
-    ]
+                    {
+                        image:
+                            "images/Otis/photoalbum/page01-photo03.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-left-photo-03",
+
+                        caption:
+                            "Dagen då jag hittade den"
+                    }
+                ]
+            },
+
+
+            /*
+               =========================
+               SIDA 2 – HÖGER
+               =========================
+            */
+            {
+                background:
+                    "images/Otis/photoalbum/otis-backpack-item-fotoalbum-openr.PNG",
+
+                backgroundClass:
+                    "background-right",
+
+                photos: [
+                    {
+                        image:
+                            "images/Otis/photoalbum/page02-photo01.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-right-photo-01",
+
+                        caption:
+                            "Mitt första äventyr"
+                    },
+
+                    {
+                        image:
+                            "images/Otis/photoalbum/page02-photo02.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-right-photo-02",
+
+                        caption:
+                            "En vanlig dag med Bosse"
+                    },
+
+                    {
+                        image:
+                            "images/Otis/photoalbum/page02-photo03.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-right-photo-03",
+
+                        caption:
+                            "Min favoritplats"
+                    }
+                ]
+            }
+
+        ]
+
+    },
+
+
+    /*
+       =========================
+       BOSSE
+       =========================
+       Samma foton som Otis tills vidare.
+       Endast bakgrunderna är ändrade.
+    */
+    bosse01: {
+
+        id: "photoalbum",
+        title: "Bosses Fotoalbum",
+
+        pages: [
+
+            /*
+               =========================
+               SIDA 1 – VÄNSTER
+               =========================
+            */
+            {
+                background:
+                    "images/Bosse/photoalbum/bosse-backpack-item-fotoalbum-openl.PNG",
+
+                backgroundClass:
+                    "background-left",
+
+                photos: [
+                    {
+                        image:
+                            "images/Otis/photoalbum/page01-photo01.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-left-photo-01",
+
+                        caption:
+                            "En sommar för länge sedan"
+                    },
+
+                    {
+                        image:
+                            "images/Otis/photoalbum/page01-photo02.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-left-photo-02",
+
+                        caption:
+                            "En dag full av bus"
+                    },
+
+                    {
+                        image:
+                            "images/Otis/photoalbum/page01-photo03.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-left-photo-03",
+
+                        caption:
+                            "Dagen då jag hittade den"
+                    }
+                ]
+            },
+
+
+            /*
+               =========================
+               SIDA 2 – HÖGER
+               =========================
+            */
+            {
+                background:
+                    "images/Bosse/photoalbum/bosse-backpack-item-fotoalbum-openr.PNG",
+
+                backgroundClass:
+                    "background-right",
+
+                photos: [
+                    {
+                        image:
+                            "images/Otis/photoalbum/page02-photo01.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-right-photo-01",
+
+                        caption:
+                            "Mitt första äventyr"
+                    },
+
+                    {
+                        image:
+                            "images/Otis/photoalbum/page02-photo02.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-right-photo-02",
+
+                        caption:
+                            "En vanlig dag med Bosse"
+                    },
+
+                    {
+                        image:
+                            "images/Otis/photoalbum/page02-photo03.PNG",
+
+                        class:
+                            "photoalbum-photo photoalbum-page-right-photo-03",
+
+                        caption:
+                            "Min favoritplats"
+                    }
+                ]
+            }
+
+        ]
+
+    }
+
 };
