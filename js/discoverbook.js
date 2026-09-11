5d27fa44250fba31f6ed8d706d89d187e30cd950
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-09-12 01:18:31 +0200

    Update discoverbook.js

--- js/discoverbook.js
+++ js/discoverbook.js
@@ -1,99 +1,200 @@
 /*
-  Otis Upptäckarbok
+  Upptäckarbok
 */
 
 const discoverBookData = {
-    id: "discoverbook",
-    title: "Otis Upptäckarbok",
-    pages: [
 
-        // Sida 0 - Innehållsförteckning
-        {
-            image: "",
-            background:
-            "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openl.PNG",
-            backgroundClass:
-            "background-left",
-            title: "",
-            text: "",
-            imageClass: "",
-            textClass: ""
-        },
+    otis01: {
+        id: "discoverbook",
+        title: "Otis Upptäckarbok",
+        pages: [
 
-        // Sida 1
-        {
-            image: "images/Otis/discoverbook/page01.PNG",
-            background:
-            "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openr.PNG",
-            backgroundClass:
-            "background-right",
-            title: "",
-            text: "",
-            imageClass:
-            "discover-image-right",
-            textClass:
-            "discover-text-right"
-        },
+            // Sida 0 - Innehållsförteckning
+            {
+                image: "",
+                background:
+                "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openl.PNG",
+                backgroundClass:
+                "background-left",
+                title: "",
+                text: "",
+                imageClass: "",
+                textClass: ""
+            },
 
-        // Sida 2
-        {
-            image: "images/Otis/discoverbook/page02.PNG",
-            background:
-            "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openl.PNG",
-            backgroundClass:
-            "background-left",
-            title: "",
-            text: "",
-            imageClass:
-            "discover-image-left",
-            textClass:
-            "discover-text-left"
-        },
+            // Sida 1
+            {
+                image: "images/Otis/discoverbook/page01.PNG",
+                background:
+                "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openr.PNG",
+                backgroundClass:
+                "background-right",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-right",
+                textClass:
+                "discover-text-right"
+            },
 
-        // Sida 3
-        {
-            image: "images/Otis/discoverbook/page03.PNG",
-            background:
-            "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openr.PNG",
-            backgroundClass:
-            "background-right",
-            title: "",
-            text: "",
-            imageClass:
-            "discover-image-right",
-            textClass:
-            "discover-text-right"
-        },
+            // Sida 2
+            {
+                image: "images/Otis/discoverbook/page02.PNG",
+                background:
+                "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openl.PNG",
+                backgroundClass:
+                "background-left",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-left",
+                textClass:
+                "discover-text-left"
+            },
 
-        // Sida 4
-        {
-            image: "images/Otis/discoverbook/page04.PNG",
-            background:
-            "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openl.PNG",
-            backgroundClass:
-            "background-left",
-            title: "",
-            text: "",
-            imageClass:
-            "discover-image-left",
-            textClass:
-            "discover-text-left"
-        },
-      
-        // Sida 5
-        {
-            image: "images/Otis/discoverbook/page05.PNG",
-            background:
-            "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openr.PNG",
-            backgroundClass:
-            "background-right",
-            title: "",
-            text: "",
-            imageClass:
-            "discover-image-right",
-            textClass:
-            "discover-text-right"
-        }
-      
-    ]
+            // Sida 3
+            {
+                image: "images/Otis/discoverbook/page03.PNG",
+                background:
+                "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openr.PNG",
+                backgroundClass:
+                "background-right",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-right",
+                textClass:
+                "discover-text-right"
+            },
+
+            // Sida 4
+            {
+                image: "images/Otis/discoverbook/page04.PNG",
+                background:
+                "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openl.PNG",
+                backgroundClass:
+                "background-left",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-left",
+                textClass:
+                "discover-text-left"
+            },
+
+            // Sida 5
+            {
+                image: "images/Otis/discoverbook/page05.PNG",
+                background:
+                "images/Otis/discoverbook/otis-backpack-item-upptackarbok-openr.PNG",
+                backgroundClass:
+                "background-right",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-right",
+                textClass:
+                "discover-text-right"
+            }
+
+        ]
+    },
+
+
+    bosse01: {
+        id: "discoverbook",
+        title: "Bosses Upptäckarbok",
+        pages: [
+
+            // Sida 0 - Innehållsförteckning
+            {
+                image: "",
+                background:
+                "images/Bosse/discoverbook/bosse-backpack-item-upptackarbok-openl.PNG",
+                backgroundClass:
+                "background-left",
+                title: "",
+                text: "",
+                imageClass: "",
+                textClass: ""
+            },
+
+            // Sida 1
+            {
+                image: "images/Otis/discoverbook/page01.PNG",
+                background:
+                "images/Bosse/discoverbook/bosse-backpack-item-upptackarbok-openr.PNG",
+                backgroundClass:
+                "background-right",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-right",
+                textClass:
+                "discover-text-right"
+            },
+
+            // Sida 2
+            {
+                image: "images/Otis/discoverbook/page02.PNG",
+                background:
+                "images/Bosse/discoverbook/bosse-backpack-item-upptackarbok-openl.PNG",
+                backgroundClass:
+                "background-left",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-left",
+                textClass:
+                "discover-text-left"
+            },
+
+            // Sida 3
+            {
+                image: "images/Otis/discoverbook/page03.PNG",
+                background:
+                "images/Bosse/discoverbook/bosse-backpack-item-upptackarbok-openr.PNG",
+                backgroundClass:
+                "background-right",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-right",
+                textClass:
+                "discover-text-right"
+            },
+
+            // Sida 4
+            {
+                image: "images/Otis/discoverbook/page04.PNG",
+                background:
+                "images/Bosse/discoverbook/bosse-backpack-item-upptackarbok-openl.PNG",
+                backgroundClass:
+                "background-left",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-left",
+                textClass:
+                "discover-text-left"
+            },
+
+            // Sida 5
+            {
+                image: "images/Otis/discoverbook/page05.PNG",
+                background:
+                "images/Bosse/discoverbook/bosse-backpack-item-upptackarbok-openr.PNG",
+                backgroundClass:
+                "background-right",
+                title: "",
+                text: "",
+                imageClass:
+                "discover-image-right",
+                textClass:
+                "discover-text-right"
+            }
+
+        ]
+    }
+
 };
