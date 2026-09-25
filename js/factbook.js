a8eb4f50669a58b4d514e1f0d4d538ccf63933da
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-09-25 11:09:10 +0200

    Update factbook.js

--- js/factbook.js
+++ js/factbook.js
@@ -1086,6 +1086,358 @@ Uttrar
 
         ]
 
+    },
+
+      /*
+      ========================================
+      DOKKA
+      ========================================
+      Samma bilder och texter som Bosse tills vidare.
+      Endast titel och bakgrunder hör till Dokka senare.
+    */
+    dokka01: {
+        id: "factbook",
+        title: "Dokka Faktabok",
+        pages: [
+            // ========================================
+            // Sida 0 - Innehållsförteckning
+            // ========================================
+            {
+                image: "",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openl.PNG",
+                backgroundClass:
+                    "background-left",
+                title: "",
+                text: "",
+                imageClass: ""
+            },
+            // ========================================
+            // KAPITEL 1 – GRÄVLINGAR
+            // ========================================
+            // Sida 1 - Kapitelstart
+            {
+                image:
+                    "images/Bosse/factbook/chapter01/page01.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openr.PNG",
+                backgroundClass:
+                    "background-right",
+                title:
+`
+<span class="chapter-number">
+Kapitel 1
+</span>
+<br>
+<span class="chapter-name">
+Grävlingar
+</span>
+`,
+                text:
+`
+<p>Hej! 🦡</p>
+<p>Nu ska vi lära känna grävlingen lite bättre.</p>
+<p>Grävlingen är duktig på att gräva och trivs bäst när det börjar bli kväll.</p>
+<p>Här kommer några spännande fakta om grävlingar!</p>
+`,
+                imageClass:
+                    "fact-chapter-image",
+                textClass:
+                    "fact-chapter-text"
+            },
+            // Sida 2 - Den randiga masken
+            {
+                image:
+                    "images/Vannen/factbook/chapter02/page02.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openl.PNG",
+                backgroundClass:
+                    "background-left",
+                title: "",
+                text:
+`
+<p>Det är lätt att känna igen en grävling.</p>
+<p>Den har en kraftig kropp, korta ben och en svartvit randig teckning över huvudet.</p>
+<p>De långa klorna är perfekta när grävlingen behöver gräva.</p>
+`,
+                imageClass:
+                    "fact-image-left",
+                textClass:
+                    "fact-text-left"
+            },
+            // Sida 3 - Grävlingens hem
+            {
+                image:
+                    "images/Vannen/factbook/chapter02/page03.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openr.PNG",
+                backgroundClass:
+                    "background-right",
+                title: "",
+                text:
+`
+<p>Grävlingen bor ofta i ett stort gryt under marken. 🌿</p>
+<p>Med sina starka klor kan den gräva gångar och rum där den kan vila och ta skydd.</p>
+<p>Ett gryt kan användas av grävlingar under många år och ha flera olika ingångar.</p>
+`,
+                imageClass:
+                    "fact-image-right",
+                textClass:
+                    "fact-text-right"
+            },
+            // Sida 4 - Vad äter grävlingen?
+            {
+                image:
+                    "images/Vannen/factbook/chapter02/page04.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openl.PNG",
+                backgroundClass:
+                    "background-left",
+                title: "",
+                text:
+`
+<p>Grävlingen äter lite av varje. 🍎</p>
+<p>Den kan äta maskar, insekter, smådjur, bär och annan mat som den hittar i naturen.</p>
+<p>Grävlingen är alltså en allätare – precis som många andra djur som kan hitta mat på många olika sätt.</p>
+`,
+                imageClass:
+                    "fact-image-left",
+                textClass:
+                    "fact-text-left"
+            },
+            // Sida 5 - En riktig grävare
+            {
+                image:
+                    "images/Vannen/factbook/chapter02/page05.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openr.PNG",
+                backgroundClass:
+                    "background-right",
+                title: "",
+                text:
+`
+<p>Grävlingen är en riktig grävare! 🐾</p>
+<p>De starka klorna hjälper den att gräva efter mat och bygga sitt gryt.</p>
+<p>När den letar efter mat kan den snabbt gräva sig ner i jorden.</p>
+`,
+                imageClass:
+                    "fact-image-right",
+                textClass:
+                    "fact-text-right"
+            },
+            // Sida 6 - När solen går ner
+            {
+                image:
+                    "images/Vannen/factbook/chapter02/page06.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openl.PNG",
+                backgroundClass:
+                    "background-left",
+                title: "",
+                text:
+`
+<p>Grävlingen är mest aktiv på kvällen och natten. 🌙</p>
+<p>När det blir mörkt lämnar den sitt gryt för att leta efter mat.</p>
+<p>Grävlingen har ett väldigt bra luktsinne och kan känna dofter på långt håll.</p>
+`,
+                imageClass:
+                    "fact-image-left",
+                textClass:
+                    "fact-text-left"
+            },
+            // Sida 7 - Små grävlingar
+            {
+                image:
+                    "images/Vannen/factbook/chapter02/page07.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openr.PNG",
+                backgroundClass:
+                    "background-right",
+                title: "",
+                text:
+`
+<p>Grävlingens ungar föds inne i grytet. 🦡</p>
+<p>När de blir större börjar de försiktigt upptäcka världen utanför.</p>
+<p>Så småningom lär de sig att hitta mat, gräva och klara sig själva.</p>
+`,
+                imageClass:
+                    "fact-image-right",
+                textClass:
+                    "fact-text-right"
+            },
+            // Sida 8 - Avslutning
+            {
+                image:
+                    "images/Bosse/factbook/chapter01/page08.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openl.PNG",
+                backgroundClass:
+                    "background-left",
+                title: "",
+                text:
+`
+<p>Nu vet du lite mer om grävlingar! 🌿</p>
+<p>Nästa gång du går genom skogen kan du tänka på att det kanske finns ett gryt alldeles i närheten, även om du inte kan se det.</p>
+<p>/ Dokka</p>
+`,
+                imageClass:
+                    "fact-image-left",
+                textClass:
+                    "fact-text-left"
+            },
+            // ========================================
+            // KAPITEL 2 – UTTRAR
+            // ========================================
+            // Sida 9 - Kapitelstart
+            {
+                image:
+                    "images/Bosse/factbook/chapter02/page01.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openr.PNG",
+                backgroundClass:
+                    "background-right",
+                title:
+`
+<span class="chapter-number">
+Kapitel 2
+</span>
+<br>
+<span class="chapter-name">
+Uttrar
+</span>
+`,
+                text:
+`
+<p>Hej igen! 🦦</p>
+<p>Nu ska vi lära känna ett annat djur som finns ute i naturen.</p>
+<p>Visste du att uttrar finns på många platser i världen? Här kommer några spännande fakta!</p>
+`,
+                imageClass:
+                    "fact-chapter-image",
+                textClass:
+                    "fact-chapter-text"
+            },
+            // Sida 10 - Fantastiska simmare
+            {
+                image:
+                    "images/Vannen/factbook/chapter01/page02.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openl.PNG",
+                backgroundClass:
+                    "background-left",
+                title: "",
+                text:
+`
+<p>Uttrar är riktiga vattenälskare. 🌊</p>
+<p>Deras långa kropp och starka svans hjälper dem att simma snabbt genom vattnet.</p>
+<p>De kan dyka efter mat och hålla andan under flera minuter.</p>
+`,
+                imageClass:
+                    "fact-image-left",
+                textClass:
+                    "fact-text-left"
+            },
+            // Sida 11 - Den mjuka pälsen
+            {
+                image:
+                    "images/Vannen/factbook/chapter01/page03.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openr.PNG",
+                backgroundClass:
+                    "background-right",
+                title: "",
+                text:
+`
+<p>En utterpäls är en av de tätaste pälsarna i hela djurriket.</p>
+<p>Under pälsen finns massor av små hårstrån som hjälper uttern att hålla värmen när den simmar i kallt vatten.</p>
+<p>Därför är det viktigt att uttrar tar hand om sin päls och håller den ren.</p>
+`,
+                imageClass:
+                    "fact-image-right",
+                textClass:
+                    "fact-text-right"
+            },
+            // Sida 12 - Favoritstenen
+            {
+                image:
+                    "images/Vannen/factbook/chapter01/page04.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openl.PNG",
+                backgroundClass:
+                    "background-left",
+                title: "",
+                text:
+`
+<p>Vissa havsuttrar använder en favoritsten som ett litet verktyg. 🖤</p>
+<p>De kan använda stenen för att öppna hårda skal från musslor och andra smådjur.</p>
+<p>Precis som Otis tycker många uttrar om fina stenar!</p>
+`,
+                imageClass:
+                    "fact-image-left",
+                textClass:
+                    "fact-text-left"
+            },
+            // Sida 13 - Lekfulla uttrar
+            {
+                image:
+                    "images/Vannen/factbook/chapter01/page05.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openr.PNG",
+                backgroundClass:
+                    "background-right",
+                title: "",
+                text:
+`
+<p>Uttrar är nyfikna och lekfulla djur.</p>
+<p>De kan rulla runt, jaga varandra och busa i vattnet.</p>
+<p>Lek hjälper dem att lära sig viktiga saker och hålla sig aktiva.</p>
+`,
+                imageClass:
+                    "fact-image-right",
+                textClass:
+                    "fact-text-right"
+            },
+            // Sida 14 - Var bor uttrar?
+            {
+                image:
+                    "images/Vannen/factbook/chapter01/page06.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openl.PNG",
+                backgroundClass:
+                    "background-left",
+                title: "",
+                text:
+`
+<p>Uttrar finns på många platser i världen.</p>
+<p>De kan leva vid sjöar, floder och hav.</p>
+<p>Där det finns rent vatten och gott om mat kan en utter hitta ett hem.</p>
+`,
+                imageClass:
+                    "fact-image-left page-6",
+                textClass:
+                    "fact-text-left"
+            },
+            // Sida 15 - Avslutning
+            {
+                image:
+                    "images/Bosse/factbook/chapter02/page07.PNG",
+                background:
+                    "images/Bosse/factbook/bosse-backpack-item-faktabok-openr.PNG",
+                backgroundClass:
+                    "background-right",
+                title: "",
+                text:
+`
+<p>Nu vet du lite mer om uttrar! 🌿</p>
+<p>Nästa gång du ser en bäck, en sjö eller havet kan du tänka på alla små äventyr som händer under vattenytan.</p>
+<p>/ Dokka</p>
+`,
+                imageClass:
+                    "fact-image-right",
+                textClass:
+                    "fact-text-right"
+            }
+        ]
     }
 
 };
