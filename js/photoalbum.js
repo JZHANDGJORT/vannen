81c62581135da06d11859e674fe059b8b3b7b3aa
JZHANDGJORT <jennifer.zetterman@hotmail.com>
2026-09-25 18:08:30 +0200

    Update photoalbum.js

--- js/photoalbum.js
+++ js/photoalbum.js
@@ -257,7 +257,7 @@ const photoAlbumData = {
                 photos: [
                     {
                         image:
-                            "images/Dokka/photoalbum/page01-photo01.PNG",
+                            "images/Dokka/photoalbum/page01-photo01.png",
                         class:
                             "photoalbum-photo photoalbum-page-left-photo-01",
                         caption:
@@ -265,7 +265,7 @@ const photoAlbumData = {
                     },
                     {
                         image:
-                            "images/Dokka/photoalbum/page01-photo02.PNG",
+                            "images/Dokka/photoalbum/page01-photo02.png",
                         class:
                             "photoalbum-photo photoalbum-page-left-photo-02",
                         caption:
@@ -273,7 +273,7 @@ const photoAlbumData = {
                     },
                     {
                         image:
-                            "images/Dokka/photoalbum/page01-photo03.PNG",
+                            "images/Dokka/photoalbum/page01-photo03.png",
                         class:
                             "photoalbum-photo photoalbum-page-left-photo-03",
                         caption:
@@ -294,7 +294,7 @@ const photoAlbumData = {
                 photos: [
                     {
                         image:
-                            "images/Dokka/photoalbum/page02-photo01.PNG",
+                            "images/Dokka/photoalbum/page02-photo01.png",
                         class:
                             "photoalbum-photo photoalbum-page-right-photo-01",
                         caption:
@@ -302,7 +302,7 @@ const photoAlbumData = {
                     },
                     {
                         image:
-                            "images/Dokka/photoalbum/page02-photo02.PNG",
+                            "images/Dokka/photoalbum/page02-photo02.png",
                         class:
                             "photoalbum-photo photoalbum-page-right-photo-02",
                         caption:
@@ -310,7 +310,7 @@ const photoAlbumData = {
                     },
                     {
                         image:
-                            "images/Dokka/photoalbum/page02-photo03.PNG",
+                            "images/Dokka/photoalbum/page02-photo03.png",
                         class:
                             "photoalbum-photo photoalbum-page-right-photo-03",
                         caption:
